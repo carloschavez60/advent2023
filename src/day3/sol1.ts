29ef0db4f1860415932782778e7a3ff472ce8230
carloschavez <carloschavez6020@gmail.com>
2023-12-07 18:11:48 -0500

    ready for day 3 part 2

--- src/day3/sol1.ts
+++ src/day3/sol1.ts
@@ -3,42 +3,42 @@ import { readFileSync } from 'node:fs';
 const buf = readFileSync(process.cwd() + '/src/day3/test-input.txt');
 // const buf = readFileSync(process.cwd() + '/src/day3/input.txt');
 const input = buf.toString();
-
-console.time('time');
-
 const lines = input.split('\n');
-const lineLen = lines[0].length;
-const ghostLine = ''.padStart(lineLen, '.');
+const ghostLine = ''.padStart(lines[0].length, '.');
 
 // fill top and bot lines
 lines.pop();
 lines.unshift(ghostLine);
 lines.push(ghostLine);
 
-let totalSum = 0;
-for (let y = 1; y < lines.length - 1; y++) {
-  let curNumber = '';
-  for (let x = 0; x <= lineLen; x++) {
-    const curChar = lines[y][x];
-    if (isCharNumber(curChar)) {
-      curNumber += curChar;
-    } else if (curNumber.length > 0) {
-      if (isPartNumber(curNumber, x, y, lines)) {
-        totalSum += Number(curNumber);
+console.time('time');
+partOne(lines);
+console.timeEnd('time');
+
+function partOne(lines: string[]) {
+  let totalSum = 0;
+  for (let y = 1; y < lines.length - 1; y++) {
+    let curNumber = '';
+    for (let x = 0; x <= lines[0].length; x++) {
+      const curChar = lines[y][x];
+      if (isNumber(curChar)) {
+        curNumber += curChar;
+      } else if (curNumber.length > 0) {
+        if (isPartNumber(curNumber, x, y, lines)) {
+          totalSum += Number(curNumber);
+        }
+
+        // console.log(Number(curNumber));
+
+        curNumber = '';
       }
-
-      // console.log(Number(curNumber));
-
-      curNumber = '';
     }
   }
+  // console.timeEnd('time');
+  console.log(totalSum);
 }
-console.timeEnd('time');
-console.log(totalSum);
 
-function isCharNumber(char: string) {
-  // const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  // return numbers.includes(number);
+function isNumber(char: string) {
   return !isNaN(parseInt(char));
 }
 
@@ -50,18 +50,27 @@ function isPartNumber(number: string, x: number, y: number, lines: string[]) {
   let curNumberIsAdjacentToSymbol = false;
   let curNumberLen = number.length;
   if (
-    (curLine[x] && curLine[x] !== '.') ||
-    (curLine[x - curNumberLen - 1] && curLine[x - curNumberLen - 1] !== '.')
+    (curLine[x] !== undefined && !isDot(curLine[x])) ||
+    (curLine[x - curNumberLen - 1] !== undefined &&
+      curLine[x - curNumberLen - 1] !== '.')
   ) {
     curNumberIsAdjacentToSymbol = true;
   }
   for (let k = x - curNumberLen - 1; k <= x; k++) {
     if (
-      (prevLine[k] && prevLine[k] !== '.') ||
-      (nextLine[k] && nextLine[k] !== '.')
+      (prevLine[k] !== undefined && !isDot(prevLine[k])) ||
+      (nextLine[k] !== undefined && !isDot(nextLine[k]))
     ) {
       curNumberIsAdjacentToSymbol = true;
     }
   }
   return curNumberIsAdjacentToSymbol;
+}
+
+function isDot(char: string) {
+  return char === '.';
+}
+
+function isAsterisk(char: string) {
+  return char === '*';
 }
