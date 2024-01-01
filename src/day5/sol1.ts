e64bdd4eb48be9361892754be757ee29e0921019
carloschavez <carloschavez6020@gmail.com>
2024-01-01 17:20:15 -0500

    day5 code reorganized

--- src/day5/sol1.ts
+++ src/day5/sol1.ts
@@ -1,60 +1,56 @@
 import { readFileSync } from 'node:fs';
 
-class Range {
-  dest: number;
-  src: number;
+class MapRange {
+  start: number;
   length: number;
+  destinationStart: number;
 
-  constructor(dest: number, src: number, length: number) {
-    this.dest = dest;
-    this.src = src;
+  constructor(start: number, length: number, destinationStart: number) {
+    this.start = start;
     this.length = length;
+    this.destinationStart = destinationStart;
   }
 }
 
 class Map {
-  id: string;
-  nextMapId: string;
-  ranges: Range[];
+  ranges: MapRange[];
 
-  constructor(id: string, nextMapId: string, ranges: Range[]) {
-    this.id = id;
-    this.nextMapId = nextMapId;
-    this.ranges = ranges;
+  constructor(ranges: MapRange[]) {
+    this.ranges = this.#addComplementRanges(ranges);
+  }
+
+  #addComplementRanges(ranges: MapRange[]): MapRange[] {
+    ranges.sort((a, b) => a.start - b.start);
+
+    let start = 0;
+    for (let i = 0; i < ranges.length; i++) {
+      const range = ranges[i];
+      if (range.start > start) {
+        ranges.splice(i, 0, new MapRange(start, range.start - start, start));
+        i++;
+      }
+      start = range.start + range.length;
+    }
+
+    ranges.push(new MapRange(start, Infinity, start));
+    return ranges;
+  }
+}
+
+class Range {
+  start: number;
+  length: number;
+
+  constructor(start: number, length: number) {
+    this.start = start;
+    this.length = length;
   }
 }
 
 // const filePath = process.cwd() + '/src/day5/test-input.txt'; // 35 46
 const filePath = process.cwd() + '/src/day5/input.txt'; // 226172555 47909639
 
-const fileStr = readFileSync(filePath, 'utf8');
-const contents = fileStr.split('\n\n');
-
-const seeds = contents
-  .shift()!
-  .split(': ')[1]
-  .split(' ')
-  .map((str) => +str);
-
-const maps = contents.map((mapAsString): Map => {
-  const lines = mapAsString.split('\n');
-  const [id, _, nextMapId] = lines.shift()!.split(' ')[0].split('-');
-
-  return new Map(
-    id,
-    nextMapId,
-    lines
-      .filter((line) => line !== '')
-      .map((line): Range => {
-        const [dest, src, length] = line.split(' ');
-        return new Range(+dest, +src, +length);
-      })
-  );
-});
-
-for (const map of maps) {
-  map.ranges = addComplementRanges(map.ranges);
-}
+const [seeds, maps] = getSeedsAndMaps(filePath);
 
 // console.log(JSON.stringify(maps, null, 4));
 // console.log(maps[1].ranges);
@@ -62,96 +58,125 @@ for (const map of maps) {
 console.time('partOne');
 partOne(seeds, maps);
 console.timeEnd('partOne');
+
+const seedRanges = getSeedRanges(seeds);
+
 console.time('partTwo');
-partTwo(seeds, maps);
+partTwo(seedRanges, maps);
 console.timeEnd('partTwo');
 
-function addComplementRanges(ranges: Range[]) {
-  ranges.sort((a, b) => a.src - b.src);
+function getSeedsAndMaps(filePath: string): [number[], Map[]] {
+  const fileStr = readFileSync(filePath, 'utf8');
+  const contents = fileStr.slice(0, -1).split('\n\n');
 
-  let start = 0;
-  for (let i = 0; i < ranges.length; i++) {
-    const range = ranges[i];
-    if (range.src > start) {
-      ranges.splice(i, 0, {
-        src: start,
-        dest: start,
-        length: range.src - start,
-      });
-      i++;
+  const seeds = contents
+    .shift()!
+    .split(': ')[1]
+    .split(' ')
+    .map((str) => +str);
+
+  const maps = getMaps(contents);
+  return [seeds, maps];
+}
+
+function getMaps(contents: string[]): Map[] {
+  const maps: Map[] = [];
+  for (const mapAsString of contents) {
+    const lines = mapAsString.split('\n');
+    lines.shift();
+
+    const ranges: MapRange[] = [];
+    for (const line of lines) {
+      const [destinationStart, start, length] = line.split(' ');
+      ranges.push(new MapRange(+start, +length, +destinationStart));
     }
-    start = range.src + range.length;
+
+    maps.push(new Map(ranges));
   }
-  ranges.push({
-    src: start,
-    dest: start,
-    length: Infinity,
-  });
-  return ranges;
+  return maps;
 }
 
 function partOne(seeds: number[], maps: Map[]) {
-  const locs: number[] = [];
+  const locations: number[] = [];
   for (const seed of seeds) {
-    const loc = maps.reduce(walkMapsToGetLocation, seed);
-    locs.push(loc);
+    const location = getLocation(seed, maps);
+    locations.push(location);
   }
-  // console.log(locs);
-  console.log(Math.min(...locs));
+
+  console.log(Math.min(...locations));
 }
 
-function walkMapsToGetLocation(value: number, map: Map): number {
-  let nextValue = value;
-  const range = map.ranges.find(
-    (range) => range.src <= value && value < range.src + range.length
-  );
-  if (range !== undefined) {
-    nextValue = value + (range.dest - range.src);
+function getLocation(seed: number, maps: Map[]): number {
+  let temp = seed;
+  for (const map of maps) {
+    temp = getNextTemp(temp, map);
   }
-  return nextValue;
+  const location = temp;
+  return location;
 }
 
-function partTwo(seeds: number[], maps: Map[]) {
-  let lowest = Infinity;
-  // const locs: number[] = [];
-  for (let i = 0; i < seeds.length; i += 2) {
-    const seedStart = seeds[i];
-    const seedRange = seeds[i + 1];
-    // console.log(seedStart, seedRange);
-    let start = seedStart;
-    let remainder = seedRange;
-    while (remainder > 0) {
-      // console.log('start', start, 'remainder', remainder);
-      const [locStart, consumed] = maps.reduce(walkMapsToGetLocInterval, [
-        start,
-        remainder,
-      ]);
-      // console.log('locStart', locStart, 'consumed', consumed);
-      remainder -= consumed;
-      start += consumed;
-      if (locStart < lowest) {
-        lowest = locStart;
-      }
-      // locs.push(locStart);
+function getNextTemp(temp: number, map: Map): number {
+  for (const range of map.ranges) {
+    if (range.start <= temp && temp < range.start + range.length) {
+      const diff = temp - range.start;
+      return range.destinationStart + diff;
     }
   }
-  console.log(lowest);
-  // console.log(Math.min(...locs));
+  return temp;
 }
 
-function walkMapsToGetLocInterval(
-  [value, valueRange]: [number, number],
-  map: Map
-): [number, number] {
-  // console.log(value, valueRange, map.id);
-  const range = map.ranges.find(
-    (range) => range.src <= value && value < range.src + range.length
-  )!;
-  // if (range !== undefined) {
-  const diff = value - range.src;
-  const nextValue = range.dest + diff;
-  return [nextValue, Math.min(valueRange, range.length - diff)];
-  // }
-  // console.log('this is never printed');
-  // return [value, 1];
+function getSeedRanges(seeds: number[]): Range[] {
+  const seedRanges: Range[] = [];
+  for (let i = 0; i < seeds.length; i += 2) {
+    seedRanges.push(new Range(seeds[i], seeds[i + 1]));
+  }
+  return seedRanges;
+}
+
+function partTwo(seedRanges: Range[], maps: Map[]) {
+  let lowestLocation = Infinity;
+  for (const seedRange of seedRanges) {
+    lowestLocation = Math.min(getLowestLocation(seedRange), lowestLocation);
+  }
+  console.log(lowestLocation);
+}
+
+function getLowestLocation(seedRange: Range): number {
+  let lowestLocation = Infinity;
+  let remainingRange = new Range(seedRange.start, seedRange.length);
+  while (remainingRange.length > 0) {
+    const consumptionRange = getConsumptionRange(remainingRange, maps);
+
+    remainingRange = new Range(
+      remainingRange.start + consumptionRange.length,
+      remainingRange.length - consumptionRange.length
+    );
+
+    lowestLocation = Math.min(consumptionRange.start, lowestLocation);
+  }
+  return lowestLocation;
+}
+
+function getConsumptionRange(remainderRange: Range, maps: Map[]): Range {
+  let tempRange = new Range(remainderRange.start, remainderRange.length);
+  for (const map of maps) {
+    tempRange = getNextTempRange(tempRange, map);
+  }
+  return new Range(tempRange.start, tempRange.length);
+}
+
+function getNextTempRange(tempRange: Range, map: Map): Range {
+  for (const mapRange of map.ranges) {
+    if (
+      mapRange.start <= tempRange.start &&
+      tempRange.start < mapRange.start + mapRange.length
+    ) {
+      const diff = tempRange.start - mapRange.start;
+      return new Range(
+        mapRange.destinationStart + diff,
+        Math.min(tempRange.length, mapRange.length - diff)
+      );
+    }
+  }
+  throw new Error('mapRange not found');
 }
