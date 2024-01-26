09fe0a5e07b858f3b63c4eea32815ea1a0ffd9db
carloschavez <carloschavez6020@gmail.com>
2024-01-26 17:50:20 -0500

    day1 refactored

--- src/day1/sol1.ts
+++ src/day1/sol1.ts
@@ -1,85 +1,84 @@
 import { readFileSync } from 'node:fs';
 
-// const buf = readFileSync(process.cwd() + '/src/day1/test-input.txt'); // 142 142
-// const buf = readFileSync(process.cwd() + '/src/day1/test-input-part-two.txt'); // NaN 281
-const buf = readFileSync(process.cwd() + '/src/day1/input.txt'); // 54573 54591
-const input = buf.toString();
-const lines = input.split('\n');
-lines.pop();
+const stringToNumber = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+} as const;
 
-console.time('partOne');
-partOne(lines);
-console.timeEnd('partOne');
-console.time('partTwo');
-partTwo(lines);
-console.timeEnd('partTwo');
+main();
 
-function partOne(lines: string[]) {
-  let sum = 0;
-  for (const line of lines) {
-    let firstDigit = '';
-    let lastDigit = '';
-    let firstDigitWasCatched = false;
+function main() {
+  const filePath = process.cwd() + '/src/day1/test-input.txt'; // 142 142
+  // const filePath = process.cwd() + '/src/day1/part-two-test-input.txt'; // 209 281
+  // const filePath = process.cwd() + '/src/day1/input.txt'; // 54573 54591
 
-    for (const char of line) {
-      if (isNumber(char)) {
-        if (!firstDigitWasCatched) {
-          firstDigit = char;
-          firstDigitWasCatched = true;
-        }
-        lastDigit = char;
-      }
-    }
+  const lines = getLines(filePath);
 
-    sum += parseInt(firstDigit + lastDigit);
-  }
-  console.log(sum);
+  console.time('partOne');
+  const calibrationValueSum = partOne(lines);
+  console.log(calibrationValueSum);
+  console.timeEnd('partOne');
+
+  console.time('partTwo');
+  const calibrationValueSum2 = partTwo(lines);
+  console.log(calibrationValueSum2);
+  console.timeEnd('partTwo');
 }
 
-function partTwo(lines: string[]) {
-  const numStrings = [
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine',
-  ];
-  let sum = 0;
-  for (const line of lines) {
-    let firstDigit = 0;
-    let lastDigit = 0;
-    let firstDigitWasCatched = false;
+function getLines(filePath: string): readonly string[] {
+  const lines = readFileSync(filePath, 'utf8').split('\n');
+  lines.pop();
+  return lines;
+}
 
-    for (let x = 0; x < line.length; x++) {
-      const char = line[x];
-      if (isNumber(char)) {
-        if (!firstDigitWasCatched) {
-          firstDigit = parseInt(char);
-          firstDigitWasCatched = true;
-        }
-        lastDigit = parseInt(char);
-      } else {
-        for (let i = 0; i < numStrings.length; i++) {
-          if (line.startsWith(numStrings[i], x)) {
-            if (!firstDigitWasCatched) {
-              firstDigit = i + 1;
-              firstDigitWasCatched = true;
-            }
-            lastDigit = i + 1;
-          }
-        }
-      }
-    }
+function partOne(lines: readonly string[]): number {
+  return lines.reduce((s, line) => s + getCalibrationValue(line), 0);
+}
 
-    sum += firstDigit * 10 + lastDigit;
+function getCalibrationValue(line: string): number {
+  const digits = line.split('').filter((char) => isNumber(char));
+  const firstDigit = digits[0];
+  const lastDigit = digits.at(-1);
+  if (firstDigit !== undefined && lastDigit !== undefined) {
+    return parseInt(firstDigit + lastDigit);
   }
-  console.log(sum);
+  return 0;
 }
 
 function isNumber(char: string): boolean {
   return !isNaN(parseInt(char));
+}
+
+function partTwo(lines: readonly string[]): number {
+  return lines.reduce((s, line) => s + getCalibrationValuePartTwo(line), 0);
+}
+
+function getCalibrationValuePartTwo(line: string): number {
+  const digits: readonly number[] = line
+    .split('')
+    .reduce((digits: number[], char, i) => {
+      if (isNumber(char)) {
+        digits.push(parseInt(char));
+      }
+      const key = Object.keys(stringToNumber).find((key) =>
+        line.startsWith(key, i)
+      );
+      if (key !== undefined) {
+        digits.push(stringToNumber[key as keyof typeof stringToNumber]);
+      }
+      return digits;
+    }, []);
+  const firstDigit = digits[0];
+  const lastDigit = digits.at(-1);
+  if (firstDigit !== undefined && lastDigit !== undefined) {
+    return firstDigit * 10 + lastDigit;
+  }
+  return 0;
 }
