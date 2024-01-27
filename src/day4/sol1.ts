5acffe9babccac48e5041d0b4ab014a3356cf91d
carloschavez <carloschavez6020@gmail.com>
2024-01-27 14:57:30 -0500

    day4 part one refactored

--- src/day4/sol1.ts
+++ src/day4/sol1.ts
@@ -1,47 +1,77 @@
-import { readFileSync } from 'node:fs';
+import { getLines } from '../utils.js';
 
-// const buf = readFileSync(process.cwd() + '/src/day4/test-input.txt'); // 13 30
-const buf = readFileSync(process.cwd() + '/src/day4/input.txt'); // 23673 12263631
-const input = buf.toString();
-const lines = input.split('\n');
-lines.pop();
-
-console.time('partOne');
-partOne(lines);
-console.timeEnd('partOne');
-console.time('partTwo');
-partTwo(lines);
-console.timeEnd('partTwo');
-
-function partOne(lines: string[]) {
-  let pileWorth = 0;
-  for (const line of lines) {
-    const myWinNumsCount = getMyWinNumsCount(line);
-    if (myWinNumsCount !== 0) {
-      pileWorth += Math.pow(2, myWinNumsCount - 1);
-    }
-  }
-  console.log(pileWorth);
+class Card {
+  constructor(
+    public readonly winningNumbers: readonly number[],
+    public readonly numbers: readonly number[]
+  ) {}
 }
 
-function partTwo(lines: string[]) {
-  const copiesArr: number[] = [];
-  let totalCards = 0;
-  for (const line of lines) {
-    const copies = copiesArr.shift() ?? 0;
-    const instances = 1 + copies;
-    totalCards += instances;
-    const myWinNumsCount = getMyWinNumsCount(line);
-    for (let i = 0; i < myWinNumsCount; i++) {
-      if (copiesArr[i] !== undefined) {
-        copiesArr[i] += instances;
-      } else {
-        copiesArr.push(instances);
-      }
-    }
-    // console.log(copiesArr);
-  }
-  console.log(totalCards);
+main();
+
+function main() {
+  // const filePath = process.cwd() + '/src/day4/test-input.txt'; // 13 30
+  const filePath = process.cwd() + '/src/day4/input.txt'; // 23673 12263631
+
+  const lines = getLines(filePath);
+  const cards = getCards(lines);
+
+  console.time('partOne');
+  const pileWorth = partOne(cards);
+  console.log(pileWorth);
+  console.timeEnd('partOne');
+
+  console.time('partTwo');
+  // const totalCards = partTwo(cards);
+  // console.log(totalCards);
+  console.timeEnd('partTwo');
+}
+
+function getCards(lines: readonly string[]): readonly Card[] {
+  return lines.map((line) => {
+    const [wnStr, nStr] = line.split(':')[1].split('|');
+    const wn = wnStr
+      .trim()
+      .split(' ')
+      .map((s) => parseInt(s));
+    const n = nStr
+      .trimStart()
+      .split(' ')
+      .map((s) => parseInt(s));
+    return new Card(wn, n);
+  });
+}
+
+function partOne(cards: readonly Card[]): number {
+  return cards.reduce((s, card) => s + getWorth(card), 0);
+}
+
+function getWorth(card: Card): number {
+  const wnCount = card.numbers.filter((n) =>
+    card.winningNumbers.some((wn) => wn === n)
+  ).length;
+  return wnCount > 0 ? Math.pow(2, wnCount - 1) : 0;
+}
+
+function partTwo(cards: readonly Card[]): number {
+  return 0;
+  // const copiesArr: number[] = [];
+  // let totalCards = 0;
+  // for (const line of lines) {
+  //   const copies = copiesArr.shift() ?? 0;
+  //   const instances = 1 + copies;
+  //   totalCards += instances;
+  //   const myWinNumsCount = getMyWinNumsCount(line);
+  //   for (let i = 0; i < myWinNumsCount; i++) {
+  //     if (copiesArr[i] !== undefined) {
+  //       copiesArr[i] += instances;
+  //     } else {
+  //       copiesArr.push(instances);
+  //     }
+  //   }
+  //   // console.log(copiesArr);
+  // }
+  // console.log(totalCards);
 }
 
 function getMyWinNumsCount(line: string) {
