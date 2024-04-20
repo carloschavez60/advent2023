166d637173019ba3bf424619098094892c9f079e
carloschavez <carloschavez6020@gmail.com>
2024-04-20 10:14:29 -0500

    refactor: day2

--- src/day2/sol1.ts
+++ src/day2/sol1.ts
@@ -1,4 +1,4 @@
-import { readFileLines } from '../utils.js';
+import { readFileSync } from 'node:fs';
 
 const config: ReadonlyMap<string, number> = new Map([
   ['red', 12],
@@ -9,17 +9,17 @@ const config: ReadonlyMap<string, number> = new Map([
 class Game {
   constructor(
     public readonly id: string,
-    public readonly sets: ReadonlyMap<string, number>[]
+    public readonly ballSets: ReadonlyMap<string, number>[]
   ) {}
 
   get isPossible(): boolean {
-    for (const set of this.sets) {
-      for (const color of set.keys()) {
+    for (const ballSet of this.ballSets) {
+      for (const color of ballSet.keys()) {
         const configBallCount = config.get(color);
         if (configBallCount === undefined) {
           continue;
         }
-        if (set.get(color)! > configBallCount) {
+        if (ballSet.get(color)! > configBallCount) {
           return false;
         }
       }
@@ -28,18 +28,21 @@ class Game {
   }
 
   get minSetPower(): number {
-    let prod = 1;
+    let power = 1;
     for (const ballCount of this.#minSet.values()) {
-      prod *= ballCount;
+      power *= ballCount;
     }
-    return prod;
+    return power;
   }
 
-  get #minSet(): ReadonlyMap<string, number> {
+  get #minSet(): Map<string, number> {
     const minSet = new Map<string, number>();
-    for (const set of this.sets) {
-      for (const color of set.keys()) {
-        minSet.set(color, Math.max(minSet.get(color) ?? 0, set.get(color)!));
+    for (const ballSet of this.ballSets) {
+      for (const color of ballSet.keys()) {
+        minSet.set(
+          color,
+          Math.max(ballSet.get(color)!, minSet.get(color) ?? 0)
+        );
       }
     }
     return minSet;
@@ -52,35 +55,43 @@ function main() {
   // const inputPath = process.cwd() + '/src/day2/test-input.txt'; // 8 2286
   const inputPath = process.cwd() + '/src/day2/input.txt'; // 2476 54911
 
-  const lines = readFileLines(inputPath);
-  const games = toGames(lines);
+  const lines: string[] = readFileLines(inputPath);
+  const games: Game[] = toGames(lines);
 
   console.time('partOne');
-  const sum = sumPossibleGameIds(games);
+  const sum: number = sumPossibleGameIds(games);
   console.log(sum);
   console.timeEnd('partOne');
 
   console.time('partTwo');
-  const sum2 = sumMinSetPowers(games);
+  const sum2: number = sumMinSetPowers(games);
   console.log(sum2);
   console.timeEnd('partTwo');
 }
 
-function toGames(lines: readonly string[]): readonly Game[] {
+function readFileLines(filePath: string): string[] {
+  const lines = readFileSync(filePath, 'utf8').split('\n');
+  lines.pop();
+  return lines;
+}
+
+function toGames(lines: readonly string[]): Game[] {
   const games: Game[] = [];
   for (const line of lines) {
-    const [strId, strSets] = line.split(':');
-    const id = strId.split(' ')[1];
-    const sets: ReadonlyMap<string, number>[] = [];
-    for (const strSet of strSets.split(';')) {
-      const set = new Map<string, number>();
-      for (const strSubset of strSet.split(',')) {
-        const [strBallCount, color] = strSubset.trimStart().split(' ');
-        set.set(color, Number(strBallCount));
+    const [idAsString, ballSetsAsString] = line.split(':');
+    const id = idAsString.split(' ')[1];
+    const ballSets: ReadonlyMap<string, number>[] = [];
+    for (const ballSetAsString of ballSetsAsString.split(';')) {
+      const ballSet = new Map<string, number>();
+      for (const ballSubsetAsString of ballSetAsString.split(',')) {
+        const [ballCountAsString, color] = ballSubsetAsString
+          .trimStart()
+          .split(' ');
+        ballSet.set(color, Number(ballCountAsString));
       }
-      sets.push(set);
+      ballSets.push(ballSet);
     }
-    games.push(new Game(id, sets));
+    games.push(new Game(id, ballSets));
   }
   return games;
 }
