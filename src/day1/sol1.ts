5ee1995e79bdf765fa64fb0128c0544c9e613afc
carloschavez <carloschavez6020@gmail.com>
2024-07-02 10:51:04 -0500

    refactor: day1

--- src/day1/sol1.ts
+++ src/day1/sol1.ts
@@ -12,22 +12,8 @@ const digitCharBySpelledDigit: ReadonlyMap<string, string> = new Map([
   ['nine', '9'],
 ]);
 
-main();
-
-function main() {
-  const testFilePath = process.cwd() + '/src/day1/test-input.txt'; // 142 142
-  const testFile2Path = process.cwd() + '/src/day1/part-two-test-input.txt'; // 209 281
-  const filePath = process.cwd() + '/src/day1/input.txt'; // 54573 54591
-
-  day1Part1(testFilePath);
-  day1Part2(testFile2Path);
-
-  day1Part1(filePath);
-  day1Part2(filePath);
-}
-
 /**
- * Promise: Every line of the file must have at least one digit character.
+ * Every line of the file must have at least one digit character.
  */
 function day1Part1(filePath: string) {
   const lines = readFileLines(filePath);
@@ -38,7 +24,7 @@ function day1Part1(filePath: string) {
 
 function readFileLines(filePath: string): string[] {
   const lines = readFileSync(filePath, 'utf8').split('\n');
-  if (lines.at(-1) === '') {
+  if (lines[lines.length - 1] === '') {
     lines.pop();
   }
   return lines;
@@ -54,9 +40,9 @@ function sumCalibrationValues(lines: readonly string[]): number {
 
 function getCalibrationValue(line: string): number {
   const [firstDigitChar, firstDigitCharIndex] = getFirstDigitChar(line);
-  const lastDigitChar = getLastDigitChar(line, firstDigitCharIndex);
+  let lastDigitChar = getLastDigitChar(line, firstDigitCharIndex);
   if (lastDigitChar === undefined) {
-    return parseInt(firstDigitChar + firstDigitChar);
+    lastDigitChar = firstDigitChar;
   }
   return parseInt(firstDigitChar + lastDigitChar);
 }
@@ -95,7 +81,7 @@ function getLastDigitChar(
 }
 
 /**
- * Promise: Every line of the file must have at least
+ * Every line of the file must have at least
  * one digit character or spelled digit.
  */
 function day1Part2(filePath: string) {
@@ -115,9 +101,9 @@ function sumPart2CalibrationValues(lines: readonly string[]): number {
 
 function getPart2CalibrationValue(line: string): number {
   const [firstDigitChar, firstDigitCharIndex] = getPart2FirstDigitChar(line);
-  const lastDigitChar = getPart2LastDigitChar(line, firstDigitCharIndex);
+  let lastDigitChar = getPart2LastDigitChar(line, firstDigitCharIndex);
   if (lastDigitChar === undefined) {
-    return parseInt(firstDigitChar + firstDigitChar);
+    lastDigitChar = firstDigitChar;
   }
   return parseInt(firstDigitChar + lastDigitChar);
 }
@@ -132,10 +118,9 @@ function getPart2FirstDigitChar(line: string): [string, number] {
       return [char, i];
     }
     const digitChar = searchSpelledDigitAndParse(line, i);
-    if (digitChar === undefined) {
-      continue;
+    if (digitChar !== undefined) {
+      return [digitChar, i];
     }
-    return [digitChar, i];
   }
   throw new Error(
     'Panic: Line must have at least one digit character or spelled digit.'
@@ -155,10 +140,9 @@ function getPart2LastDigitChar(
       return char;
     }
     const digitChar = searchSpelledDigitAndParse(line, i);
-    if (digitChar === undefined) {
-      continue;
+    if (digitChar !== undefined) {
+      return digitChar;
     }
-    return digitChar;
   }
   return undefined;
 }
@@ -177,3 +161,13 @@ function searchSpelledDigitAndParse(
   }
   return undefined;
 }
+
+const testFilePath = process.cwd() + '/src/day1/test-input.txt';
+const testFile2Path = process.cwd() + '/src/day1/part-two-test-input.txt';
+const filePath = process.cwd() + '/src/day1/input.txt';
+
+day1Part1(testFilePath); // 142
+day1Part2(testFile2Path); // 281
+
+day1Part1(filePath); // 54573
+day1Part2(filePath); // 54591
