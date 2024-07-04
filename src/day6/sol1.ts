a6c22f4ae44dd162643ae9c1cee0088ec6d66fe4
carloschavez <carloschavez6020@gmail.com>
2024-07-04 18:17:25 -0500

    refactor: day6

--- src/day6/sol1.ts
+++ src/day6/sol1.ts
@@ -10,26 +10,32 @@ class Race {
   }
 }
 
-main();
+function day6Part1(filePath: string) {
+  const races: Race[] = readRacesFromFile(filePath);
 
-function main() {
-  // const inputPath = process.cwd() + '/src/day6/test-input.txt'; // 288 71503
-  const inputPath = process.cwd() + '/src/day6/input.txt'; // 1312850 36749103
+  const part1Result: number = multiplyWaysToWin(races);
+  console.log('part 1 result:', part1Result);
+}
 
-  const l = readFileLines(inputPath);
-  const rs = toRaces(l);
-
-  console.time('partOne');
-  const p = multiplyWaysToWin(rs);
-  console.log(p);
-  console.timeEnd('partOne');
-
-  const r = toRace(l);
-
-  console.time('partTwo');
-  const c = countWaysToWin(r);
-  console.log(c);
-  console.timeEnd('partTwo');
+function readRacesFromFile(filePath: string): Race[] {
+  const lines: string[] = readFileLines(filePath);
+  const times: number[] = [];
+  const distances: number[] = [];
+  for (const timeStr of lines[0].split(':')[1].trimStart().split(' ')) {
+    if (timeStr !== '') {
+      times.push(parseInt(timeStr));
+    }
+  }
+  for (const distanceStr of lines[1].split(':')[1].trimStart().split(' ')) {
+    if (distanceStr !== '') {
+      distances.push(parseInt(distanceStr));
+    }
+  }
+  const races: Race[] = [];
+  for (let i = 0; i < times.length; i++) {
+    races.push(new Race(times[i], distances[i]));
+  }
+  return races;
 }
 
 function readFileLines(filePath: string): string[] {
@@ -40,78 +46,81 @@ function readFileLines(filePath: string): string[] {
   return lines;
 }
 
-function toRaces(lines: string[]): Race[] {
-  const times: number[] = [];
-  const distances: number[] = [];
-  for (const s of lines[0].split(':')[1].trimStart().split(' ')) {
-    if (s !== '') {
-      times.push(Number(s));
-    }
-  }
-  for (const s of lines[1].split(':')[1].trimStart().split(' ')) {
-    if (s !== '') {
-      distances.push(Number(s));
-    }
-  }
-  const races: Race[] = [];
-  for (let i = 0; i < times.length; i++) {
-    races.push(new Race(times[i], distances[i]));
-  }
-  return races;
-}
-
-function multiplyWaysToWin(races: Race[]): number {
+function multiplyWaysToWin(races: readonly Race[]): number {
   let prod = 1;
-  for (const r of races) {
-    let count = 0;
-    for (let i = 0; i <= r.time; i++) {
-      const boatSpeed = i;
-      const movingTime = r.time - i;
-      const distance = boatSpeed * movingTime;
-      if (distance > r.recordDistance) {
-        count++;
-      }
-    }
-    prod *= count;
+  for (const race of races) {
+    prod *= countWaysToWin(race);
   }
   return prod;
 }
 
-function toRace(lines: string[]): Race {
-  let stime = '';
-  let sdistance = '';
-  for (const s of lines[0].split(':')[1].trimStart().split(' ')) {
-    if (s !== '') {
-      stime += s;
+function countWaysToWin(race: Race): number {
+  let count = 0;
+  for (let i = 0; i <= race.time; i++) {
+    const boatSpeed: number = i;
+    const movingTime: number = race.time - i;
+    const distance: number = boatSpeed * movingTime;
+    if (distance > race.recordDistance) {
+      count++;
     }
   }
-  for (const s of lines[1].split(':')[1].trimStart().split(' ')) {
-    if (s !== '') {
-      sdistance += s;
-    }
-  }
-
-  return new Race(Number(stime), Number(sdistance));
+  return count;
 }
 
-function countWaysToWin(r: Race): number {
-  let start = 0;
-  let final = 0;
-  for (let i = 0; start === 0; i++) {
-    const boatSpeed = i;
-    const movingTime = r.time - i;
-    const distance = boatSpeed * movingTime;
-    if (distance > r.recordDistance) {
+function day6Part2(filePath: string) {
+  const race: Race = readRaceFromFile(filePath);
+
+  const part2Result: number = countPart2WaysToWin(race);
+  console.log('part 2 result:', part2Result);
+}
+
+function readRaceFromFile(filePath: string): Race {
+  const lines: string[] = readFileLines(filePath);
+  let timeStr = '';
+  let distanceStr = '';
+  for (const partialTimeStr of lines[0].split(':')[1].trimStart().split(' ')) {
+    if (partialTimeStr !== '') {
+      timeStr += partialTimeStr;
+    }
+  }
+  for (const partialDistanceStr of lines[1]
+    .split(':')[1]
+    .trimStart()
+    .split(' ')) {
+    if (partialDistanceStr !== '') {
+      distanceStr += partialDistanceStr;
+    }
+  }
+  return new Race(parseInt(timeStr), parseInt(distanceStr));
+}
+
+function countPart2WaysToWin(race: Race): number {
+  let start: number | undefined;
+  let end: number | undefined;
+  for (let i = 0; start === undefined; i++) {
+    const boatSpeed: number = i;
+    const movingTime: number = race.time - i;
+    const distance: number = boatSpeed * movingTime;
+    if (distance > race.recordDistance) {
       start = i;
     }
   }
-  for (let i = r.time; final === 0; i--) {
-    const boatSpeed = i;
-    const movingTime = r.time - i;
-    const distance = boatSpeed * movingTime;
-    if (distance > r.recordDistance) {
-      final = i;
+  for (let i = race.time; end === undefined; i--) {
+    const boatSpeed: number = i;
+    const movingTime: number = race.time - i;
+    const distance: number = boatSpeed * movingTime;
+    if (distance > race.recordDistance) {
+      end = i;
     }
   }
-  return final - start + 1;
+  return end - start + 1;
 }
+
+const testFilePath = process.cwd() + '/src/day6/test-input.txt';
+const filePath = process.cwd() + '/src/day6/input.txt';
+
+day6Part1(testFilePath); // 288
+day6Part2(testFilePath); // 71503
+
+day6Part1(filePath); // 1312850
+day6Part2(filePath); // 36749103
