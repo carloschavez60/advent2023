f307de85ddcbc064fa4c94ee2722ddc9ddb5cc03
carloschavez <carloschavez6020@gmail.com>
2024-07-29 16:41:13 -0500

    refactor: day2

--- src/day2/sol1.ts
+++ src/day2/sol1.ts
@@ -7,74 +7,33 @@ const ballCountLimitByBallColor: ReadonlyMap<string, number> = new Map([
 ]);
 
 class Game {
-  readonly id: string;
-  readonly ballSets: readonly ReadonlyMap<string, number>[];
+  id: string;
+  ballSets: Map<string, number>[];
 
-  constructor(id: string, ballSets: readonly ReadonlyMap<string, number>[]) {
+  constructor(id: string, ballSets: Map<string, number>[]) {
     this.id = id;
     this.ballSets = ballSets;
-  }
-
-  isPossible(): boolean {
-    for (const ballSet of this.ballSets) {
-      for (const [ballColor, ballCount] of ballSet.entries()) {
-        const ballCountLimit: number | undefined =
-          ballCountLimitByBallColor.get(ballColor);
-        if (ballCountLimit === undefined) {
-          continue;
-        }
-        if (ballCount > ballCountLimit) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
-  minBallSetPower(): number {
-    let power = 1;
-    for (const ballCount of this.#minBallSet().values()) {
-      power *= ballCount;
-    }
-    return power;
-  }
-
-  #minBallSet(): Map<string, number> {
-    const minBallSet = new Map<string, number>();
-    for (const ballSet of this.ballSets) {
-      for (const [ballColor, ballCount] of ballSet.entries()) {
-        const maxBallCount: number | undefined = minBallSet.get(ballColor);
-        if (maxBallCount === undefined) {
-          minBallSet.set(ballColor, ballCount);
-        } else {
-          const newMaxBallCount = Math.max(maxBallCount, ballCount);
-          minBallSet.set(ballColor, newMaxBallCount);
-        }
-      }
-    }
-    return minBallSet;
   }
 }
 
 function day2Part1(filePath: string) {
-  const games: Game[] = readGamesFromFile(filePath);
-
-  const part1Result: number = sumPossibleGameIds(games);
+  let games = readGamesFromFile(filePath);
+  let part1Result = sumPossibleGameIds(games);
   console.log('part 1 result:', part1Result);
 }
 
 function readGamesFromFile(filePath: string): Game[] {
-  const lines: string[] = readFileLines(filePath);
-  const games: Game[] = [];
-  for (const line of lines) {
-    const game: Game = parseLineToGame(line);
+  let lines = readFileLines(filePath);
+  let games: Game[] = [];
+  for (let line of lines) {
+    let game = parseLineToGame(line);
     games.push(game);
   }
   return games;
 }
 
 function readFileLines(filePath: string): string[] {
-  const lines: string[] = readFileSync(filePath, 'utf8').split('\n');
+  let lines = readFileSync(filePath, 'utf8').split('\n');
   if (lines[lines.length - 1] === '') {
     lines.pop();
   }
@@ -82,52 +41,91 @@ function readFileLines(filePath: string): string[] {
 }
 
 function parseLineToGame(line: string): Game {
-  const [idStr, ballSetsStr] = line.split(':');
-  const id: string = idStr.split(' ')[1];
-  const ballSets: Map<string, number>[] = [];
-  for (const ballSetStr of ballSetsStr.split(';')) {
+  let [idStr, ballSetsStr] = line.split(':');
+  let id = idStr.split(' ')[1];
+  let ballSets: Map<string, number>[] = [];
+  for (let ballSetStr of ballSetsStr.split(';')) {
     ballSets.push(getBallSet(ballSetStr));
   }
   return new Game(id, ballSets);
 }
 
 function getBallSet(ballSetStr: string): Map<string, number> {
-  const ballSet = new Map<string, number>();
-  for (const ballSubsetStr of ballSetStr.split(',')) {
-    const [ballCountStr, ballColor] = ballSubsetStr.trimStart().split(' ');
-    const ballCount: number = parseInt(ballCountStr);
+  let ballSet = new Map<string, number>();
+  for (let ballSubsetStr of ballSetStr.split(',')) {
+    let [ballCountStr, ballColor] = ballSubsetStr.trimStart().split(' ');
+    let ballCount = parseInt(ballCountStr);
     ballSet.set(ballColor, ballCount);
   }
   return ballSet;
 }
 
-function sumPossibleGameIds(games: readonly Game[]): number {
+function sumPossibleGameIds(games: Game[]): number {
   let sum = 0;
-  for (const game of games) {
-    if (game.isPossible()) {
+  for (let game of games) {
+    if (isPossible(game)) {
       sum += parseInt(game.id);
     }
   }
   return sum;
 }
 
-function day2Part2(filePath: string) {
-  const games: Game[] = readGamesFromFile(filePath);
+function isPossible(game: Game): boolean {
+  for (let ballSet of game.ballSets) {
+    for (let [ballColor, ballCount] of ballSet.entries()) {
+      let ballCountLimit = ballCountLimitByBallColor.get(ballColor);
+      if (ballCountLimit === undefined) {
+        continue;
+      }
+      if (ballCount > ballCountLimit) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
-  const part2Result: number = sumMinBallSetPowers(games);
+function day2Part2(filePath: string) {
+  let games = readGamesFromFile(filePath);
+  let part2Result = sumMinBallSetPowers(games);
   console.log('part 2 result:', part2Result);
 }
 
-function sumMinBallSetPowers(games: readonly Game[]): number {
+function sumMinBallSetPowers(games: Game[]): number {
   let sum = 0;
-  for (const game of games) {
-    sum += game.minBallSetPower();
+  for (let game of games) {
+    sum += getMinBallSetPower(game);
   }
   return sum;
 }
 
-const testFilePath = process.cwd() + '/src/day2/test-input.txt';
-const filePath = process.cwd() + '/src/day2/input.txt';
+function getMinBallSetPower(game: Game): number {
+  let minBallSet = getMinBallSet(game);
+  let power = 1;
+  for (let ballCount of minBallSet.values()) {
+    power *= ballCount;
+  }
+  return power;
+}
+
+function getMinBallSet(game: Game): Map<string, number> {
+  let minBallSet = new Map<string, number>();
+  for (let ballSet of game.ballSets) {
+    for (let [ballColor, ballCount] of ballSet.entries()) {
+      let maxBallCount = minBallSet.get(ballColor);
+      if (maxBallCount === undefined) {
+        minBallSet.set(ballColor, ballCount);
+      } else {
+        let newMaxBallCount = Math.max(maxBallCount, ballCount);
+        minBallSet.set(ballColor, newMaxBallCount);
+      }
+    }
+  }
+  return minBallSet;
+}
+
+let testFilePath = process.cwd() + '/src/day2/test-input.txt';
+let filePath = process.cwd() + '/src/day2/input.txt';
 
 day2Part1(testFilePath); // 8
 day2Part2(testFilePath); // 2286
