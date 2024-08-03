698d1100bc8479724c54265fc8ff464f94a86e34
carloschavez <carloschavez6020@gmail.com>
2024-08-03 17:24:54 -0500

    refactor: day1 default solution is now procedural instead of oop

--- src/day1/sol1.ts
+++ src/day1/sol1.ts
@@ -1,181 +1,155 @@
 import { readFileSync } from 'node:fs';
 
-class FileReader {
-  private filePath: string;
+const digitCharBySpelledDigit: ReadonlyMap<string, string> = new Map([
+  ['one', '1'],
+  ['two', '2'],
+  ['three', '3'],
+  ['four', '4'],
+  ['five', '5'],
+  ['six', '6'],
+  ['seven', '7'],
+  ['eight', '8'],
+  ['nine', '9'],
+]);
 
-  public constructor(filePath: string) {
-    this.filePath = filePath;
+function readFileLines(filePath: string): string[] {
+  const lines = readFileSync(filePath, 'utf8').split('\n');
+  if (lines[lines.length - 1] === '') {
+    lines.pop();
   }
-
-  public readFileLines(): string[] {
-    const lines = readFileSync(this.filePath, 'utf8').split('\n');
-    if (lines[lines.length - 1] === '') {
-      lines.pop();
-    }
-    return lines;
-  }
+  return lines;
 }
 
-class Day1 {
-  private fileReader: FileReader;
+function day1Part1(filePath: string): number {
+  const lines = readFileLines(filePath);
+  return sumCalibrationValues(lines);
+}
 
-  private static readonly digitCharBySpelledDigit: ReadonlyMap<string, string> =
-    new Map([
-      ['one', '1'],
-      ['two', '2'],
-      ['three', '3'],
-      ['four', '4'],
-      ['five', '5'],
-      ['six', '6'],
-      ['seven', '7'],
-      ['eight', '8'],
-      ['nine', '9'],
-    ]);
-
-  public constructor(fileReader: FileReader) {
-    this.fileReader = fileReader;
+function sumCalibrationValues(lines: string[]): number {
+  let sum = 0;
+  for (const line of lines) {
+    sum += getCalibrationValue(line);
   }
+  return sum;
+}
 
-  public part1(): number {
-    const lines = this.fileReader.readFileLines();
-    return this.sumCalibrationValues(lines);
+/**
+ * Returns 0 if there are no digit characters on the line.
+ */
+function getCalibrationValue(line: string): number {
+  const firstDigitChar = searchFirstDigitChar(line);
+  if (firstDigitChar === undefined) {
+    return 0;
   }
+  const lastDigitChar = searchLastDigitChar(line);
+  if (lastDigitChar === undefined) {
+    return 0;
+  }
+  return parseInt(firstDigitChar + lastDigitChar);
+}
 
-  private sumCalibrationValues(lines: string[]): number {
-    let sum = 0;
-    for (const line of lines) {
-      sum += this.getCalibrationValue(line);
+function searchFirstDigitChar(line: string): string | undefined {
+  for (const char of line) {
+    const charIsDigit = checkCharIsDigit(char);
+    if (charIsDigit) {
+      return char;
     }
-    return sum;
   }
+  return undefined;
+}
 
-  /**
-   * Returns 0 if there are no digit characters on the line.
-   */
-  private getCalibrationValue(line: string): number {
-    const firstDigitChar = this.searchFirstDigitChar(line);
-    if (firstDigitChar === undefined) {
-      return 0;
-    }
-    const lastDigitChar = this.searchLastDigitChar(line);
-    if (lastDigitChar === undefined) {
-      return 0;
-    }
-    return parseInt(firstDigitChar + lastDigitChar);
-  }
+function checkCharIsDigit(char: string): boolean {
+  return '0' <= char && char <= '9';
+}
 
-  private searchFirstDigitChar(line: string): string | undefined {
-    for (const char of line) {
-      const charIsDigit = this.checkCharIsDigit(char);
-      if (charIsDigit) {
-        return char;
-      }
+function searchLastDigitChar(line: string): string | undefined {
+  for (let i = line.length - 1; i >= 0; i--) {
+    const char = line.charAt(i);
+    const charIsDigit = checkCharIsDigit(char);
+    if (charIsDigit) {
+      return char;
     }
-    return undefined;
   }
+  return undefined;
+}
 
-  private checkCharIsDigit(char: string): boolean {
-    return '0' <= char && char <= '9';
+function day1Part2(filePath: string): number {
+  const lines = readFileLines(filePath);
+  return sumPart2CalibrationValues(lines);
+}
+
+function sumPart2CalibrationValues(lines: string[]): number {
+  let sum = 0;
+  for (const line of lines) {
+    sum += getPart2CalibrationValue(line);
   }
+  return sum;
+}
 
-  private searchLastDigitChar(line: string): string | undefined {
-    for (let i = line.length - 1; i >= 0; i--) {
-      const char = line.charAt(i);
-      const charIsDigit = this.checkCharIsDigit(char);
-      if (charIsDigit) {
-        return char;
-      }
+/**
+ * Returns 0 if there are no digit characters on the line.
+ */
+function getPart2CalibrationValue(line: string): number {
+  const firstDigitChar = searchPart2FirstDigitChar(line);
+  if (firstDigitChar === undefined) {
+    return 0;
+  }
+  const lastDigitChar = searchPart2LastDigitChar(line);
+  if (lastDigitChar === undefined) {
+    return 0;
+  }
+  return parseInt(firstDigitChar + lastDigitChar);
+}
+
+function searchPart2FirstDigitChar(line: string): string | undefined {
+  for (let i = 0; i < line.length; i++) {
+    const char = line.charAt(i);
+    const charIsDigit = checkCharIsDigit(char);
+    if (charIsDigit) {
+      return char;
     }
-    return undefined;
-  }
-
-  public part2(): number {
-    const lines = this.fileReader.readFileLines();
-    return this.sumPart2CalibrationValues(lines);
-  }
-
-  private sumPart2CalibrationValues(lines: string[]): number {
-    let sum = 0;
-    for (const line of lines) {
-      sum += this.getPart2CalibrationValue(line);
+    const digitChar = searchDigitCharAsSpelledDigit(line, i);
+    if (digitChar !== undefined) {
+      return digitChar;
     }
-    return sum;
   }
+  return undefined;
+}
 
-  /**
-   * Returns 0 if there are no digit characters on the line.
-   */
-  private getPart2CalibrationValue(line: string): number {
-    const firstDigitChar = this.searchPart2FirstDigitChar(line);
-    if (firstDigitChar === undefined) {
-      return 0;
+function searchPart2LastDigitChar(line: string): string | undefined {
+  for (let i = line.length - 1; i >= 0; i--) {
+    const char = line.charAt(i);
+    const charIsDigit = checkCharIsDigit(char);
+    if (charIsDigit) {
+      return char;
     }
-    const lastDigitChar = this.searchPart2LastDigitChar(line);
-    if (lastDigitChar === undefined) {
-      return 0;
+    const digitChar = searchDigitCharAsSpelledDigit(line, i);
+    if (digitChar !== undefined) {
+      return digitChar;
     }
-    return parseInt(firstDigitChar + lastDigitChar);
   }
+  return undefined;
+}
 
-  private searchPart2FirstDigitChar(line: string): string | undefined {
-    for (let i = 0; i < line.length; i++) {
-      const char = line.charAt(i);
-      const charIsDigit = this.checkCharIsDigit(char);
-      if (charIsDigit) {
-        return char;
-      }
-      const digitChar = this.searchDigitCharAsSpelledDigit(line, i);
-      if (digitChar !== undefined) {
-        return digitChar;
-      }
+function searchDigitCharAsSpelledDigit(
+  line: string,
+  position: number
+): string | undefined {
+  for (const [spelledDigit, digitChar] of digitCharBySpelledDigit.entries()) {
+    if (line.startsWith(spelledDigit, position)) {
+      return digitChar;
     }
-    return undefined;
   }
-
-  private searchPart2LastDigitChar(line: string): string | undefined {
-    for (let i = line.length - 1; i >= 0; i--) {
-      const char = line.charAt(i);
-      const charIsDigit = this.checkCharIsDigit(char);
-      if (charIsDigit) {
-        return char;
-      }
-      const digitChar = this.searchDigitCharAsSpelledDigit(line, i);
-      if (digitChar !== undefined) {
-        return digitChar;
-      }
-    }
-    return undefined;
-  }
-
-  private searchDigitCharAsSpelledDigit(
-    line: string,
-    position: number
-  ): string | undefined {
-    for (const [
-      spelledDigit,
-      digitChar,
-    ] of Day1.digitCharBySpelledDigit.entries()) {
-      if (line.startsWith(spelledDigit, position)) {
-        return digitChar;
-      }
-    }
-    return undefined;
-  }
+  return undefined;
 }
 
 const part1TestFilePath = process.cwd() + '/src/day1/test-input.txt';
 const part2TestFilePath = process.cwd() + '/src/day1/part-two-test-input.txt';
 const filePath = process.cwd() + '/src/day1/input.txt';
 
-const part1TestFileReader = new FileReader(part1TestFilePath);
-const part1TestDay1 = new Day1(part1TestFileReader);
-console.log(`Part 1 test result: ${part1TestDay1.part1()}`); // 142
+console.log(`Part 1 test result: ${day1Part1(part1TestFilePath)}`); // 142
 
-const part2TestFileReader = new FileReader(part2TestFilePath);
-const part2TestDay1 = new Day1(part2TestFileReader);
-console.log(`Part 2 test result: ${part2TestDay1.part2()}`); // 281
+console.log(`Part 2 test result: ${day1Part2(part2TestFilePath)}`); // 281
 
-const fileReader = new FileReader(filePath);
-const day1 = new Day1(fileReader);
-console.log(`Part 1 result: ${day1.part1()}`); // 54573
-console.log(`Part 2 result: ${day1.part2()}`); // 54591
+console.log(`Part 1 result: ${day1Part1(filePath)}`); // 54573
+console.log(`Part 2 result: ${day1Part2(filePath)}`); // 54591
---
+++ src/day1/sol1_OOP.ts
@@ -0,0 +1,181 @@
+import { readFileSync } from 'node:fs';
+
+class FileReader {
+  private filePath: string;
+
+  public constructor(filePath: string) {
+    this.filePath = filePath;
+  }
+
+  public readFileLines(): string[] {
+    const lines = readFileSync(this.filePath, 'utf8').split('\n');
+    if (lines[lines.length - 1] === '') {
+      lines.pop();
+    }
+    return lines;
+  }
+}
+
+class Day1 {
+  private fileReader: FileReader;
+
+  private static readonly digitCharBySpelledDigit: ReadonlyMap<string, string> =
+    new Map([
+      ['one', '1'],
+      ['two', '2'],
+      ['three', '3'],
+      ['four', '4'],
+      ['five', '5'],
+      ['six', '6'],
+      ['seven', '7'],
+      ['eight', '8'],
+      ['nine', '9'],
+    ]);
+
+  public constructor(fileReader: FileReader) {
+    this.fileReader = fileReader;
+  }
+
+  public part1(): number {
+    const lines = this.fileReader.readFileLines();
+    return this.sumCalibrationValues(lines);
+  }
+
+  private sumCalibrationValues(lines: string[]): number {
+    let sum = 0;
+    for (const line of lines) {
+      sum += this.getCalibrationValue(line);
+    }
+    return sum;
+  }
+
+  /**
+   * Returns 0 if there are no digit characters on the line.
+   */
+  private getCalibrationValue(line: string): number {
+    const firstDigitChar = this.searchFirstDigitChar(line);
+    if (firstDigitChar === undefined) {
+      return 0;
+    }
+    const lastDigitChar = this.searchLastDigitChar(line);
+    if (lastDigitChar === undefined) {
+      return 0;
+    }
+    return parseInt(firstDigitChar + lastDigitChar);
+  }
+
+  private searchFirstDigitChar(line: string): string | undefined {
+    for (const char of line) {
+      const charIsDigit = this.checkCharIsDigit(char);
+      if (charIsDigit) {
+        return char;
+      }
+    }
+    return undefined;
+  }
+
+  private checkCharIsDigit(char: string): boolean {
+    return '0' <= char && char <= '9';
+  }
+
+  private searchLastDigitChar(line: string): string | undefined {
+    for (let i = line.length - 1; i >= 0; i--) {
+      const char = line.charAt(i);
+      const charIsDigit = this.checkCharIsDigit(char);
+      if (charIsDigit) {
+        return char;
+      }
+    }
+    return undefined;
+  }
+
+  public part2(): number {
+    const lines = this.fileReader.readFileLines();
+    return this.sumPart2CalibrationValues(lines);
+  }
+
+  private sumPart2CalibrationValues(lines: string[]): number {
+    let sum = 0;
+    for (const line of lines) {
+      sum += this.getPart2CalibrationValue(line);
+    }
+    return sum;
+  }
+
+  /**
+   * Returns 0 if there are no digit characters on the line.
+   */
+  private getPart2CalibrationValue(line: string): number {
+    const firstDigitChar = this.searchPart2FirstDigitChar(line);
+    if (firstDigitChar === undefined) {
+      return 0;
+    }
+    const lastDigitChar = this.searchPart2LastDigitChar(line);
+    if (lastDigitChar === undefined) {
+      return 0;
+    }
+    return parseInt(firstDigitChar + lastDigitChar);
+  }
+
+  private searchPart2FirstDigitChar(line: string): string | undefined {
+    for (let i = 0; i < line.length; i++) {
+      const char = line.charAt(i);
+      const charIsDigit = this.checkCharIsDigit(char);
+      if (charIsDigit) {
+        return char;
+      }
+      const digitChar = this.searchDigitCharAsSpelledDigit(line, i);
+      if (digitChar !== undefined) {
+        return digitChar;
+      }
+    }
+    return undefined;
+  }
+
+  private searchPart2LastDigitChar(line: string): string | undefined {
+    for (let i = line.length - 1; i >= 0; i--) {
+      const char = line.charAt(i);
+      const charIsDigit = this.checkCharIsDigit(char);
+      if (charIsDigit) {
+        return char;
+      }
+      const digitChar = this.searchDigitCharAsSpelledDigit(line, i);
+      if (digitChar !== undefined) {
+        return digitChar;
+      }
+    }
+    return undefined;
+  }
+
+  private searchDigitCharAsSpelledDigit(
+    line: string,
+    position: number
+  ): string | undefined {
+    for (const [
+      spelledDigit,
+      digitChar,
+    ] of Day1.digitCharBySpelledDigit.entries()) {
+      if (line.startsWith(spelledDigit, position)) {
+        return digitChar;
+      }
+    }
+    return undefined;
+  }
+}
+
+const part1TestFilePath = process.cwd() + '/src/day1/test-input.txt';
+const part2TestFilePath = process.cwd() + '/src/day1/part-two-test-input.txt';
+const filePath = process.cwd() + '/src/day1/input.txt';
+
+const part1TestFileReader = new FileReader(part1TestFilePath);
+const part1TestDay1 = new Day1(part1TestFileReader);
+console.log(`Part 1 test result: ${part1TestDay1.part1()}`); // 142
+
+const part2TestFileReader = new FileReader(part2TestFilePath);
+const part2TestDay1 = new Day1(part2TestFileReader);
+console.log(`Part 2 test result: ${part2TestDay1.part2()}`); // 281
+
+const fileReader = new FileReader(filePath);
+const day1 = new Day1(fileReader);
+console.log(`Part 1 result: ${day1.part1()}`); // 54573
+console.log(`Part 2 result: ${day1.part2()}`); // 54591
